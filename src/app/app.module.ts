cc19f6dfa8744c0db7feb33107211bace3bdcb22
Matt Belas <belasmatthewkim@gmail.com>
2023-09-19 19:19:22 +0800

    added config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, Injector, NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule {}
+export class AppModule implements DoBootstrap {
+  constructor(readonly injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define('from-remote-component', el);
+  }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method, @angular-eslint/use-lifecycle-interface
+  ngDoBootstrap(appRef: ApplicationRef) {
+    console.log('do customc boostrap', appRef);
+  }
+}
